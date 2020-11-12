4301273da45773f10e1999f4ea8ceba633bd31c5
andre <andrebertonhadev@gmail.com>
2020-11-12 08:38:02 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+// View Container pode ser div footer header main aside section, 
+// pode representar qualquer um desses containers
+// elementos nao possuem estilizacao propria
 
 export default function App() {
-    return <View />;
+    return <View />
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundCOlor: '#7159c1',
+    },
+});
 
