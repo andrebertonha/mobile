425ad90fafe95c04e01427060400e38d7e461ead
andre <andrebertonhadev@gmail.com>
2020-11-12 09:33:24 -0800

    diferencas do reactjs

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 // View Container pode ser div footer header main aside section, 
 // pode representar qualquer um desses containers
 // elementos nao possuem estilizacao propria
 
 export default function App() {
-    return <View />
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <View style={styles.container}>            
+                <Text>Texto from index js</Text>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundCOlor: '#7159c1',
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    
 });
 
